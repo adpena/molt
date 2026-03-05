a1e47c781c7f2a9c2b0e73f18affd05c58303693
Alejandro Peña <adpena@gmail.com>
2026-03-05 12:57:26 -0600

    chore: sync all changes

--- include/numpy/ndarraytypes.h
+++ include/numpy/ndarraytypes.h
@@ -18,6 +18,8 @@ typedef long npy_long;
 typedef unsigned long npy_ulong;
 typedef long long npy_longlong;
 typedef unsigned long long npy_ulonglong;
+typedef uint64_t npy_uint64;
+typedef intptr_t npy_hash_t;
 typedef float npy_float;
 typedef double npy_double;
 typedef long double npy_longdouble;
@@ -26,18 +28,53 @@ typedef size_t npy_uintp;
 
 #define _MOLT_NUMPY_OBJECT_HEAD PyObject *ob_base
 
+typedef struct NpyAuxData_tag NpyAuxData;
+typedef struct PyArray_ArrFuncs PyArray_ArrFuncs;
+
 typedef struct PyArray_Descr {
     _MOLT_NUMPY_OBJECT_HEAD;
     PyTypeObject *typeobj;
     char kind;
     char type;
     char byteorder;
-    char flags;
+    npy_uint64 flags;
     int type_num;
     int elsize;
     int alignment;
     void *subarray;
+    PyObject *names;
+    PyObject *fields;
+    PyObject *metadata;
 } PyArray_Descr;
+
+typedef struct _arr_descr {
+    PyArray_Descr *base;
+    PyObject *shape;
+} PyArray_ArrayDescr;
+
+typedef struct {
+    PyArray_Descr base;
+    PyObject *fields;
+    PyObject *names;
+    PyArray_ArrFuncs *f;
+    PyObject *metadata;
+    NpyAuxData *c_metadata;
+    npy_hash_t hash;
+} PyArray_DescrProto;
+
+typedef struct {
+    void *ctx;
+    void *(*malloc)(void *ctx, size_t size);
+    void *(*calloc)(void *ctx, size_t nelem, size_t elsize);
+    void *(*realloc)(void *ctx, void *ptr, size_t new_size);
+    void (*free)(void *ctx, void *ptr, size_t size);
+} PyDataMemAllocator;
+
+typedef struct {
+    char name[127];
+    uint8_t version;
+    PyDataMemAllocator allocator;
+} PyDataMem_Handler;
 
 typedef struct PyArrayObject_fields {
     _MOLT_NUMPY_OBJECT_HEAD;
@@ -48,6 +85,8 @@ typedef struct PyArrayObject_fields {
     PyObject *base;
     PyArray_Descr *descr;
     int flags;
+    PyObject *weakreflist;
+    void *mem_handler;
 } PyArrayObject_fields;
 
 typedef PyArrayObject_fields PyArrayObject;
@@ -64,9 +103,9 @@ typedef struct PyArray_DatetimeMetaData {
 
 typedef PyArray_DatetimeMetaData PyArray_DatetimeDTypeMetaData;
 
-typedef struct PyArray_ArrFuncs {
+struct PyArray_ArrFuncs {
     int _molt_reserved;
-} PyArray_ArrFuncs;
+};
 
 typedef struct PyArrayFlagsObject {
     _MOLT_NUMPY_OBJECT_HEAD;
@@ -95,23 +134,44 @@ typedef struct PyArrayMultiIterObject {
 typedef struct PyArray_DTypeMeta {
     _MOLT_NUMPY_OBJECT_HEAD;
     PyArray_Descr *singleton;
+    int type_num;
+    PyTypeObject *scalar_type;
+    npy_uint64 flags;
+    void *dt_slots;
+    void *reserved[3];
 } PyArray_DTypeMeta;
 
 typedef struct PyArrayMethodObject {
     _MOLT_NUMPY_OBJECT_HEAD;
-    int _molt_reserved;
+    const char *name;
+    int nin;
+    int nout;
+    int casting;
+    int flags;
+    PyArray_DTypeMeta **dtypes;
+    void *slots;
 } PyArrayMethodObject;
 
+typedef struct PyBoundArrayMethodObject {
+    _MOLT_NUMPY_OBJECT_HEAD;
+    PyArrayMethodObject *method;
+} PyBoundArrayMethodObject;
+
 typedef struct PyArrayMethod_Context {
-    PyArray_DTypeMeta *descriptors[3];
+    PyObject *caller;
+    PyArrayMethodObject *method;
+    PyArray_Descr *const *descriptors;
+    void *_reserved;
+    npy_uint64 flags;
+    void *parameters;
 } PyArrayMethod_Context;
 
 typedef int (*PyArrayMethod_StridedLoop)(
     PyArrayMethod_Context *context,
-    char **data,
-    npy_intp *dimensions,
-    npy_intp *strides,
-    void *auxdata
+    char *const *data,
+    const npy_intp *dimensions,
+    const npy_intp *strides,
+    NpyAuxData *auxdata
 );
 
 typedef struct PyArrayMethod_Spec {
@@ -136,6 +196,14 @@ typedef struct PyUFuncObject {
     int nargs;
 } PyUFuncObject;
 
+typedef struct {
+    _MOLT_NUMPY_OBJECT_HEAD;
+    PyObject *base;
+    void *ptr;
+    npy_intp len;
+    int flags;
+} PyArray_Chunk;
+
 typedef struct PyVoidScalarObject {
     _MOLT_NUMPY_OBJECT_HEAD;
     int _molt_reserved;
@@ -145,6 +213,76 @@ typedef struct PyDatetimeScalarObject {
     _MOLT_NUMPY_OBJECT_HEAD;
     int _molt_reserved;
 } PyDatetimeScalarObject;
+
+typedef enum {
+    NPY_NO_CASTING = 0,
+    NPY_EQUIV_CASTING = 1,
+    NPY_SAFE_CASTING = 2,
+    NPY_SAME_KIND_CASTING = 3,
+    NPY_UNSAFE_CASTING = 4,
+} NPY_CASTING;
+
+typedef void (NpyAuxData_FreeFunc)(NpyAuxData *);
+typedef NpyAuxData *(NpyAuxData_CloneFunc)(NpyAuxData *);
+
+struct NpyAuxData_tag {
+    NpyAuxData_FreeFunc *free;
+    NpyAuxData_CloneFunc *clone;
+};
+
+typedef enum {
+    NPY_BOOL_SCALAR,
+    NPY_INTPOS_SCALAR,
+    NPY_INTNEG_SCALAR,
+    NPY_FLOAT_SCALAR,
+    NPY_COMPLEX_SCALAR,
+    NPY_OBJECT_SCALAR
+} NPY_SCALARKIND;
+
+typedef enum {
+    NPY_ANYORDER = -1,
+    NPY_CORDER = 0,
+    NPY_FORTRANORDER = 1,
+    NPY_KEEPORDER = 2
+} NPY_ORDER;
+
+typedef enum {
+    NPY_COPY_NEVER = 0,
+    NPY_COPY_ALWAYS = 1,
+    NPY_COPY_IF_NEEDED = 2
+} NPY_COPYMODE;
+
+typedef void (PyArray_CopySwapFunc)(void *, void *, int, void *);
+typedef void (PyArray_CopySwapNFunc)(
+    void *,
+    npy_intp,
+    void *,
+    npy_intp,
+    npy_intp,
+    int,
+    void *
+);
+typedef int (PyArray_CompareFunc)(const void *, const void *, void *);
+typedef void (PyArray_VectorUnaryFunc)(void *, void *, npy_intp, void *, void *);
+
+typedef struct npy_unpacked_static_string {
+    size_t size;
+    const char *buf;
+} npy_static_string;
+
+typedef struct npy_string_allocator npy_string_allocator;
+
+typedef struct {
+    PyArray_Descr base;
+    PyObject *na_object;
+    char coerce;
+    char has_nan_na;
+    char has_string_na;
+    char array_owned;
+    npy_static_string default_string;
+    npy_static_string na_name;
+    npy_string_allocator *allocator;
+} PyArray_StringDTypeObject;
 
 #define NPY_API_VERSION 0x00000012
 #define NPY_FEATURE_VERSION 0x00000012
@@ -195,6 +333,7 @@ typedef struct PyDatetimeScalarObject {
 #define PyTypeNum_ISDATETIME(t) ((t) == NPY_DATETIME || (t) == NPY_TIMEDELTA)
 #define PyTypeNum_ISUSERDEF(t) ((t) >= 256)
 #define PyTypeNum_ISEXTENDED(t) PyTypeNum_ISUSERDEF(t)
+#define PyTypeNum_ISOBJECT(t) ((t) == NPY_OBJECT)
 #define PyTypeNum_ISFLEXIBLE(t) ((t) == NPY_STRING || (t) == NPY_UNICODE || (t) == NPY_VOID)
 
 #define PyDataType_SUBARRAY(descr) ((descr) != NULL ? (descr)->subarray : NULL)
