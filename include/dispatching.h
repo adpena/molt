cd89c546c0d0ae0a8ef30a78e8dd62e3a70e52f9
Alejandro Peña <adpena@gmail.com>
2026-03-08 14:59:36 -0500

    chore(numpy): capture latest generated header deltas

---
+++ include/dispatching.h
@@ -0,0 +1,7 @@
+#ifndef MOLT_NUMPY_COMPAT_DISPATCHING_H
+#define MOLT_NUMPY_COMPAT_DISPATCHING_H
+
+#include <numpy/arrayobject.h>
+#include <numpy/ufuncobject.h>
+
+#endif
--- include/npy_sort.h
+++ include/npy_sort.h
@@ -3,4 +3,27 @@
 
 #include <numpy/ndarraytypes.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+NPY_NO_EXPORT int npy_quicksort(void *start, npy_intp num, void *varr);
+NPY_NO_EXPORT int npy_heapsort(void *start, npy_intp num, void *varr);
+NPY_NO_EXPORT int npy_mergesort(void *start, npy_intp num, void *varr);
+NPY_NO_EXPORT int npy_timsort(void *start, npy_intp num, void *varr);
+
+NPY_NO_EXPORT int npy_aquicksort(void *start, npy_intp *arg, npy_intp num, void *varr);
+NPY_NO_EXPORT int npy_aheapsort(void *start, npy_intp *arg, npy_intp num, void *varr);
+NPY_NO_EXPORT int npy_amergesort(void *start, npy_intp *arg, npy_intp num, void *varr);
+NPY_NO_EXPORT int npy_atimsort(void *start, npy_intp *arg, npy_intp num, void *varr);
+
+NPY_NO_EXPORT int npy_quicksort_impl(void *start, npy_intp num, void *varr);
+NPY_NO_EXPORT int npy_aquicksort_impl(void *start, npy_intp *arg, npy_intp num, void *varr);
+NPY_NO_EXPORT int npy_mergesort_impl(void *start, npy_intp num, void *varr);
+NPY_NO_EXPORT int npy_amergesort_impl(void *start, npy_intp *arg, npy_intp num, void *varr);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
